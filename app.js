2fe8b8a8db02f282177a061fdc8376c5c750be04
lgoodman320 <lgoodman320@gmail.com>
2023-12-26 11:16:03 -0500

    add Get All Tours endpoint

--- app.js
+++ app.js
@@ -11,15 +11,27 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello from the server side!', app: 'Natours' });
-})
-
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
-})
-
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Hello from the server side!', app: 'Natours' });
+// })
+
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...');
+// })
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
