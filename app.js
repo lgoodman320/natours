b1f31029884343d262df51534979db5b4d39649a
lgoodman320 <lgoodman320@gmail.com>
2023-12-28 13:39:05 -0500

    add Create Tour and Get Tour By ID endpoints

--- app.js
+++ app.js
@@ -7,20 +7,16 @@ import { dirname } from 'path';
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware
+app.use(express.json());
+
 // ES modules don't support __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({ message: 'Hello from the server side!', app: 'Natours' });
-// })
-
-// app.post('/', (req, res) => {
-//     res.send('You can post to this endpoint...');
-// })
-
+// Routes
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -31,7 +27,41 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
 
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',        
+        data: {
+            tour
+        }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    }
+    );
+})
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
