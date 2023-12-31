3a90276d1840858c2ec65f1fd9a193212d422600
lgoodman320 <lgoodman320@gmail.com>
2023-12-31 18:48:42 -0500

    Add static file serving middleware

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 import express from 'express';
-
 import 'dotenv/config';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// ES modules don't support __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app  = express();
 
 
 // MIDDLE WARES
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👋🏾');
