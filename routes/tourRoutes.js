f279edbba194e7429afe54cff60657c434dc87de
lgoodman320 <lgoodman320@gmail.com>
2023-12-31 18:23:10 -0500

    Add checkID middleware to tourController.js and tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,19 @@
 import express from 'express';
-import { getAllTours, createTour, getTour, updateTour, deleteTour } from '../controllers/tourController.js';
+import {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  checkID,
+} from '../controllers/tourController.js';
 
 const router = express.Router();
 
-router
-    .route('/')
-    .get(getAllTours)
-    .post(createTour);
+router.param('id', checkID);
 
-router
-    .route('/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
+router.route('/').get(getAllTours).post(createTour);
+
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 export default router;
