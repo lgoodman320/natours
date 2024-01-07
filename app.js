1f2ba0b0cdb477f6d36930bccb9f27e4d0e85c71
lgoodman320 <lgoodman320@gmail.com>
2024-01-06 20:10:41 -0500

    Update .gitignore and app.js, add start:dev and start:prod scripts

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
+
 import express from 'express';
 
-import 'dotenv/config';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import morgan from 'morgan';
 
 const app  = express();
 
-
 // MIDDLE WARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import app from './app.js';
+
 const PORT = process.env.PORT || 3000;
 
 // START SERVER
 app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
+  console.log(`App is running on port ${PORT}`);
 });
