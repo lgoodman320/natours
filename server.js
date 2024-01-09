fd5b14cd8661a8176880bd2871953cc6e88828d2
lgoodman320 <lgoodman320@gmail.com>
2024-01-08 23:59:33 -0500

    Refactor server.js and add test tour creation

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
 import mongoose from 'mongoose';
 import app from './app.js';
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
-mongoose.connect(DB)
-.then( () => console.log('DB connection successful!'));
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
   },
   rating: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price']
-  }
+    required: [true, 'A tour must have a price'],
+  },
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR ⭐️:', err);
+  });
 
 const PORT = process.env.PORT || 3000;
 
