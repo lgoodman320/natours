7e6fdfc92ccb5fcfd747abae4d4679f50f4fac1e
lgoodman320 <lgoodman320@gmail.com>
2024-01-28 18:10:47 -0500

    Add route for handling unhandled routes

--- app.js
+++ app.js
@@ -24,10 +24,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋🏾');
-  next();
-});
+
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -38,4 +35,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+
+// Handling unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  })
+})
 export default app;
