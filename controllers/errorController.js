b4fa169945ca24447f2df8c6947f6adedb8a60d4
lgoodman320 <lgoodman320@gmail.com>
2024-01-30 10:39:35 -0500

    Add error handling for CastError in globalErrorHandler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+import AppError from '../utils/appError.js';
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -34,7 +41,14 @@ const globalErrorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // let error = { ...err }; // Create a copy of the err object
+    let error = Object.create(err);
+
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
   }
 };
 
