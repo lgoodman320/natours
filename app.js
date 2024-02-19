de64958a1977527c4639fb8b4b62f508a81c1c9a
lgoodman320 <lgoodman320@gmail.com>
2024-02-19 13:53:44 -0500

    Add rate limiting middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import userRouter from './routes/userRoutes.js';
 import tourRouter from './routes/tourRoutes.js';
 import AppError from './utils/appError.js';
 import globalErrorHandler from './controllers/errorController.js';
+import rateLimit from 'express-rate-limit';
 
 dotenv.config({ path: './config.env' });
 
@@ -16,10 +17,17 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-// MIDDLE WARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -27,7 +35,7 @@ app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
-  
+
   next();
 });
 
